13153351cb95625cea0f38b86ac751f6772dbf07
Sacramento-20 <sacramento-15@hotmail.com>
2023-12-13 17:34:44 +0000

    TP3

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# TP3-CLOUD
+# TP3 Cloud Computing
+
+Comandos
+
+TASK 1
+
+Criando arquivos pyfile.yaml, outputkey.yaml com o configmap.
+```
+kubectl create configmap pyfile --from-file pyfile=lambda_function.py --output yaml > pyfile.yaml
+kubectl create configmap outputkey --from-literal REDIS_OUTPUT_KEY=lucassacramento-proj3-output --output yaml > outputkey.yaml
+```
+
+Criando pod.
+```
+kubectl -n lucassacramento apply -f deployment.yaml
+```
+
+Deletando arquivos no namespace se existirem.
+```
+kubectl delete configmap pyfile
+kubectl delete configmap outputkey
+```
+
+Logs do pod.
+```
+kubectl get pods
+kubectl logs <name pod>
+```
+
+Deletando pod.
+```
+kubectl delete deploy {deployment/name}
+```
---
+++ task1/lambda_function.py
@@ -0,0 +1,51 @@
+
+def handler(input: dict, context: object) -> dict:
+  
+  resultado = dict()
+
+  # Quantidade de nucleos da maquina
+  n_cpu = 16
+
+  # uso de memoria
+  memoria_cache = input['virtual_memory-cached']
+  memoria_buffer = input['virtual_memory-buffers']
+  memoria_total = input['virtual_memory-total']
+
+  #trafego de rede
+  bytes_enviados = input['net_io_counters_eth0-bytes_sent']
+  bytes_recebidos = input['net_io_counters_eth0-bytes_recv']
+  
+  # Calculo da porcentagem de Rede, Memoria
+  percentagem_memoria =  ((memoria_cache + memoria_buffer) * 100) / memoria_total
+  porcentagem_rede = (bytes_enviados) / (bytes_enviados + bytes_recebidos) if bytes_enviados + bytes_recebidos > 0 else 0
+
+  #Calculo para cada CPU
+  porcentagem_total_cpu = {}
+  for i in range(n_cpu):
+    porcentagem_total_cpu[i] = input[f'cpu_percent-{i}']
+
+  # Media movel
+  def mean(total_utilization):
+    result = sum(total_utilization) / len(total_utilization)
+    return result
+  
+  media_movel = {}
+  for i in range(n_cpu):
+    utilization =  context.env.get(f'utilization-cpu{i}', [])
+    utilization.append(porcentagem_total_cpu[i])
+    if len(utilization) > 60:
+      utilization.pop(0)
+    
+    media_movel[i] = mean(utilization) 
+    context.env[f'utilization-cpu{i}'] = utilization
+    
+
+  # retornado os resultados
+  resultado['use-memory-percent'] = percentagem_memoria
+  resultado['use-net-percent'] = porcentagem_rede
+
+  for i in range(n_cpu):
+    # Media movel por CPU
+    resultado[f'Avg_cpu-{i}'] = media_movel[i]
+  
+  return resultado
